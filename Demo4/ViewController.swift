895938b755c2bd68001f1d236b601bf130d2d8aa
giza <giza@michaldemacbook-pro.local>
2016-05-25 20:52:06 +0800

    Initial Commit

---
+++ Demo4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo4
+//
+//  Created by giza on 2016/5/25.
+//  Copyright © 2016年 giza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
